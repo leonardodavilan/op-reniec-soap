402bb18e17ec7c453842b39e255b84d292d96000
Leonardo Jonathan Davila Napa <leonardodavila@primacia.onmicrosoft.com>
2023-11-24 16:31:57 -0500

    Correcion main

--- src/main/java/pe/com/prima/opreniecsoap/OpReniecSoapApplication.java
+++ src/main/java/pe/com/prima/opreniecsoap/OpReniecSoapApplication.java
@@ -2,10 +2,16 @@ package pe.com.prima.opreniecsoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OpReniecSoapApplication {
+public class OpReniecSoapApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(OpReniecSoapApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OpReniecSoapApplication.class, args);
 	}
